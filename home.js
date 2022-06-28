41966b06fd0908902a9b79f492bf4a6bfa26d4e5
Jyotirani12 <jyoti.lohan@gmail.com>
2022-06-28 22:26:14 +0530

    first

---
+++ home.js
@@ -0,0 +1,10 @@
+let a=document.getElementById('coupon');
+
+function getCoupon(){
+    document.getElementById('coupon').style.visibility='visible';
+}
+
+function closeCoupon()
+{
+document.getElementById('coupon').style.visibility='hidden';
+}
